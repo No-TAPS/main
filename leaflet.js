ddab968d60062c6430d9d62a89793d910c90dad1
Daniel Peralta <dannyland11@gmail.com>
2024-02-23 18:48:39 -0800

    started working on colors

--- leaflet.js
+++ leaflet.js
@@ -23,25 +23,24 @@ map.on('click', function () {
     tooltip.closeTooltip();
 });
 
-// var fuserguide = L.popup()
-//     .setLatLng(ucscCoordinates)
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
-
-// // Add a marker for UCSC
-// L.marker(ucscCoordinates).addTo(map)
-//     .bindPopup('University of California, Santa Cruz (UCSC)')
-//     .openPopup();
-
-
-
-
-///////////// Ramdom RGB color
+////////////////////// COLOR LOGIC //////////////////////
+var color = 'rgb(0,255,50)';
+var avail_zero = 'rgb(255,0,0)';
+var avail_one = 'rgb(255,90,0)';
+var avail_two = 'rgb(255, 255, 0)';
+var avail_three = 'rgb(100,255,0)';
+var avail_four = 'rgb(0,255,25)';
+/// Ramdom RGB Color ///
 function getRandomRGBColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
     return `rgb(${r},${g},${b})`;
+}
+
+function pick_random_color() {
+    colors = [avail_zero, avail_one, avail_two, avail_three, avail_four];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 // text box setting
@@ -65,9 +64,7 @@ function createTextBox(content, latlng) {
     return textBox;
 }
 
-
-
-//////////// coordinate
+//////////////// DISPLAY PARKING LOTS //////////////////
 function readjson() {
     var jsonURL = 'http://localhost:3000/ParkingCordsJson';
     var xhr = new XMLHttpRequest();
@@ -114,7 +111,7 @@ function readjson() {
                     //     .bindPopup(popupContent)
                     //     .addTo(map);
 
-                    L.polygon(coordinates, { fillColor: 'rgb(0,255,0)', fillOpacity: 0.3 })
+                    L.polygon(coordinates, { fillColor: pick_random_color(), fillOpacity: 0.3 })
                     .addTo(map)
                     .bindPopup(popupContent) 
                     //right click function
@@ -175,42 +172,6 @@ function get_menu_function(key, area) {
     }
 }
 
-// excel handle files
-function handleFile() {
-    //console.log("starting");
-
-    var url = 'http://localhost:3000/ParkingCordsExcel';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'arraybuffer';
-
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            console.log('Excel file loaded successfully.');
-
-            var data = new Uint8Array(xhr.response);
-            var workbook = XLSX.read(data, { type: 'array' });
-            var sheetName = workbook.SheetNames[0];
-            var sheet = workbook.Sheets[sheetName];
-            var jsonData = XLSX.utils.sheet_to_json(sheet);
-
-            // Ensure the map is cleared before adding new polygons
-            map.eachLayer(function (layer) {
-                if (layer instanceof L.Polygon) {
-                    map.removeLayer(layer);
-                }
-            });
-
-            //processDataAndDisplayPolygons(jsonData);
-        } else {
-            alert('Failed to load the Excel file. Please check the server route and try again.');
-        }
-    };
-
-    xhr.send();
-}
-
 function processDataAndDisplayPolygons(data) {
     console.log('Processing data and displaying polygons...');
 
@@ -268,70 +229,6 @@ function processDataAndDisplayPolygons(data) {
     console.log('Polygons displayed.');
 }
 
-
-
-
-// var westremote = L.polygon([
-//     [36.98874277751547, -122.06693840060198],
-//     [36.98869870002297, -122.0647122482799],
-//     [36.988368765753805, -122.06470151944461],
-//     [36.988418496559866, -122.06689780232429]
-    
-// ]).addTo(map);
-
-// var n165 = L.polygon([
-//                     [37.000281608809644, -122.05894317125187],
-//     	            [37.00024049224933, -122.05905527882427],
-//                     [37.00030618934344, -122.05910900092745],
-//                     [37.00025524058154, -122.05924834263263],
-//                     [37.00067757797115, -122.05930542236729],
-//                     [37.00083578629834, -122.05935242920809],
-//                     [37.00099667578452, -122.05946323104668],
-//                     [37.00134526849852, -122.0594447640743],
-//                     [37.00139487579278, -122.0592886342119],
-//                     [37.00127555008433, -122.05917111711115],
-//                     [37.001180357530934, -122.05919294171555],
-//                     [37.00100069802089, -122.0590351330374],
-//                     [37.00086528273348, -122.05896798040662],
-//                     [37.00046707995032, -122.0589478346173]
-// ]).addTo(map);											
-
-// var circle = L.circle([36.988506, -122.065921], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 50
-// }).addTo(map);
-
-
-//////////// pop up window
-// var popup = L.popup();
-
-//tool for get the coordinates
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
-
-///////////// icon
-// var warningicon = L.icon({
-//     iconUrl: 'download.png',
-//     iconSize: [12, 12],
-//     iconAnchor: [8, 8]
-//     // popupAnchor: [-3, -76]
-//     // shadowUrl: 'my-icon-shadow.png',
-//     // shadowSize: [68, 95],
-//     // shadowAnchor: [22, 94]
-// });
-
-// L.marker([36.98853587549755, -122.06590851341495], {icon: warningicon}).addTo(map);
-
-//handleFile();
-
 function toggleTextBox(boxId) {
     var currentBox = document.getElementById(boxId);
     currentBox.style.display = (currentBox.style.display === 'none' || currentBox.style.display === '') ? 'block' : 'none';
@@ -353,7 +250,6 @@ function submitTapsData(parkingLotId) {
 }
 
 function submitAvailabilityData(parkingLotId, value) {
-    console.log("bruh");
     fetch('http://localhost:3000/submitAvailabilityData', {
         method: 'POST',
         headers: {
@@ -365,11 +261,6 @@ function submitAvailabilityData(parkingLotId, value) {
     .then(data => console.log('Availability data submitted successfully:', data))
     .catch(error => console.error('Error submitting availability data:', error));
 }
-
-function bruh() {
-    console.log("bruh")
-}
-
 
 readjson();
 
