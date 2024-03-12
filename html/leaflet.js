ecdbf44205219ed921f28b6f2e8cc68a2d0e05cd
Daniel Peralta <dannyland11@gmail.com>
2024-03-11 19:12:37 -0700

    UI clean up

--- html/leaflet.js
+++ html/leaflet.js
@@ -43,7 +43,7 @@ var wicon = L.icon({
 });
 
 /////////////////////// AUTO UPDATE LOGIC ///////////////////////
-setInterval(reset_map, 60000);
+setInterval(reset_map, 600000);
 async function reset_map() {
     map.eachLayer(await function(layer){
         if(layer instanceof L.Polygon && !(layer instanceof L.Rectangle) ){
@@ -185,14 +185,13 @@ function readjson() {
 
                     // information on the popup
                     var popupContent = `
-                        <b>Area Number: ${key}</b><br>
-                        Name: ${area.name}<br>
+                        <b>${area.name} (${key})</b><br>
                         Address: ${area.address}<br>
                         Permits: ${area.permits.join(', ')}<br>
-                        R/C permit After 5: ${area.r_c_after_5}<br>
+                        R/C permit After 5: ${get_icon(area.r_c_after_5)}<br>
                         Parkmobile Hourly: ${area.parkmobile_hourly}<br>
-                        Parkmobile Daily: ${area.parkmobile_daily}<br>
-                        Parkmobile Evening/Weekend: ${area.parkmobile_eve_wknd}
+                        Parkmobile Daily: ${get_icon(area.parkmobile_daily)}<br>
+                        Parkmobile Evening/Weekend: ${get_icon(area.parkmobile_eve_wknd)}
                     `;
 
                     var polygon = L.polygon(coordinates, { fillColor: await get_color(key), fillOpacity: 0.3 })
@@ -271,6 +270,13 @@ function get_menu_function(key, area) {
                 .openOn(map);
         });
     }
+}
+
+function get_icon(value) {
+    if (value) {
+        return `<img class="bool-icon" src="check.png">`
+    }
+    return `<img class="bool-icon" src="remove.png">`
 }
 
 function processDataAndDisplayPolygons(data) {
